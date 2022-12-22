b5497b96c12196192d7b6705694d4e15f6505394
Jaron Rosenau <jaron.rosenau@gmail.com>
2022-12-21 21:19:59 -0800

    switch to htmlcoin

--- src/components/ConnectMetamask/ConnectToMetamask.jsx
+++ src/components/ConnectMetamask/ConnectToMetamask.jsx
@@ -5,10 +5,10 @@ const ConnectToMetamask = ({ connectToMetamask }) => {
   return (
     <div className="jumbotron">
       <h1 className="display-5">
-        CryptoBoy NFT Marketplace
+        CryptoBoy NFT Marketplace for Htmlcoin
       </h1>
       <p className="lead">
-        This is an NFT marketplace where you can mint your ERC721 implemented{" "}
+        This is a Web3 marketplace where you can mint HRC721 implemented{" "}
         <i>Crypto Boy NFTs</i> and manage them.
       </p>
       <hr className="my-4" />
@@ -17,7 +17,7 @@ const ConnectToMetamask = ({ connectToMetamask }) => {
         className="btn btn-primary d-flex align-items-center"
         style={{ fontSize: "0.9rem", letterSpacing: "0.14rem" }}
       >
-        Connect Metamask{" "}
+        Connect Altmasq{" "}
         <img
           src={metamaskIcon}
           alt="metamask-icon"
